b6fb26253e4c092e0fad91219c6f4645ff0d0c05
Tlangelani Mboweni <mbowenit@gmail.com>
2017-07-10 20:55:17 +0200

    get tasks from mongodb

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ### Hapi.js App
+
+
+```
+npm install
+npm run dev
+```
--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const Hapi = require('hapi');
 const PORT = 8009;
 
+require('dotenv').config();
+
+const mongoUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds133981.mlab.com:33981/${process.env.MONGO_DB}`;
+
+const mongoose = require('mongoose');
+mongoose.connect(mongoUrl, {useMongoClient: true})
+    .then(() => {
+        console.log('Mongodb connected!');
+    })
+    .catch(error => console.log(error));
+
+// create Task Model
+const Task = mongoose.model('Task', {
+    task: String
+});
+
 const server = new Hapi.Server();
 
 server.connection({
@@ -27,20 +43,27 @@ server.route({
     }
 });
 
-const tasks = [
-    { task: 'Cook supper tonight.' },
-    { task: 'Read a book.' },
-    { task: 'Clean your room.' }
-];
-
-
 server.route({
     method: 'GET',
     path: '/tasks',
     handler: (request, reply) => {
-        reply.view('tasks', {
-            title: 'Tasks',
-            tasks: tasks
+        let tasks = Task.find((error, tasks) => {
+            reply.view('tasks', { title: 'Tasks', tasks: tasks })
+        });
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/tasks',
+    handler: (request, reply) => {
+        let task = request.payload.task;
+        let newTask = new Task({
+            task: task
+        });
+        newTask.save((error, task) => {
+            if (error) return console.log(error);
+            return reply.redirect().location('tasks');
         });
     }
 });
